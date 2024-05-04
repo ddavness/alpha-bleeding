4fefe6871d15c501c0a619b772f55532dd450b55
David Duque <david@duqued.net>
2024-05-04 01:29:57 +0100

    Rearrange alpha functions

--- src/alpha-bleed.cpp
+++ src/alpha-bleed.cpp
@@ -5,50 +5,6 @@
 
 #include "png.h"
 
-void alpha_bleeding(unsigned char *image, int width, int height);
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		std::cout << "Usage: alpha-bleeding <input> <output>" << std::endl;
-		return 0;
-	}
-
-	const char *input = argv[1];
-	const char *output = argv[2];
-
-	if (std::ifstream(output).good())
-	{
-		std::cout << "Output file already exists!" << std::endl;
-		return 0;
-	}
-
-	int w, h, c;
-
-	unsigned char *data = png_load(input, &w, &h, &c);
-
-	if (data == 0)
-	{
-		std::cout << "Error loading image. Must be PNG format." << std::endl;
-		return 1;
-	}
-
-	if (c != 4)
-	{
-		std::cout << "The image must be 32 bits (RGB with alpha channel)." << std::endl;
-		delete[] data;
-		return 0;
-	}
-
-	alpha_bleeding(data, w, h);
-	png_save(output, w, h, data);
-
-	delete[] data;
-
-	return 0;
-}
-
 void alpha_bleeding(unsigned char *image, int width, int height)
 {
 	const size_t N = width * height;
---
+++ src/alpha.h
@@ -0,0 +1,2 @@
+void alpha_bleeding(unsigned char* image, int width, int height);
+void alpha_set(unsigned char* image, int width, int height, unsigned char alpha);
